a51c9fed10f6616d5fa8cc991534bd0158beed81
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-26 15:53:06 +0200

    ticket with 2 decimals

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ const age = prompt("quanti anni hai?", 32);
 console.log(age);
 const distance = prompt("quanti km desidera percorrere?", 513);
 console.log(distance);
+
+let ticket = distance * 0.21;
+console.log(ticket.toFixed(2));
