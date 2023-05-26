3eb09061a00a9f5b18122426c88a785ae53010c9
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-26 16:43:22 +0200

    meme done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 console.log("JS OK");
+//step 0
+const yourTicketPlaceholder = document.getElementById("your-ticket");
 const age = prompt("quanti anni hai?", 68);
 console.log(age);
 const distance = prompt("quanti km desidera percorrere?", 513);
 console.log(distance);
-
 let ticket = distance * 0.21;
 console.log(ticket);
 let price = ticket;
-
+//steps from 1 to 4
 if (age < 18) {
   price = ticket - (ticket / 100) * 20;
 }
-
 if (age > 65) {
   price = ticket - (ticket / 100) * 40;
 }
 console.log("il tuo biglietto costa", price.toFixed(2), "euro");
+//bonus
+yourTicketPlaceholder.innerText = price.toFixed(2);
