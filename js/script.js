43f3c02bcbe696e724a1125dec4d88b85dfde9f1
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-26 16:12:27 +0200

    ticket under 18

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 console.log("JS OK");
-const age = prompt("quanti anni hai?", 32);
+const age = prompt("quanti anni hai?", 68);
 console.log(age);
 const distance = prompt("quanti km desidera percorrere?", 513);
 console.log(distance);
 
 let ticket = distance * 0.21;
-console.log(ticket.toFixed(2));
+console.log(ticket);
+let price = ticket;
+
+if (age < 18) {
+  price = ticket - (ticket / 100) * 20;
+}
+console.log("il tuo biglietto costa", price.toFixed(2), "euro");
