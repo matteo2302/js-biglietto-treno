25cd6f5ced34a27bf90808bf671ab95409433cef
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-26 15:35:21 +0200

    prompt km

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 console.log("JS OK");
-const age = prompt("quanti anni hai?", "32");
+const age = prompt("quanti anni hai?", 32);
 console.log(age);
+const distance = prompt("quanti km desidera percorrere?", 513);
+console.log(distance);
